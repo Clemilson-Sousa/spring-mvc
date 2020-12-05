5023612b6a8942409f99ae0782454651c3661139
Clemilson <klemilsondantas@gmail.com>
2020-12-05 10:11:56 -0300

    Formulário completo

--- src/main/java/com/webapp/controller/AlunoController.java
+++ src/main/java/com/webapp/controller/AlunoController.java
@@ -1,15 +1,19 @@
 package com.webapp.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.webapp.model.Aluno;
 
 @Controller
-@RequestMapping("aluno")
+@RequestMapping("/aluno")
 public class AlunoController {
 	
 	@GetMapping("/formAluno")
@@ -22,6 +26,21 @@ public class AlunoController {
 		System.out.println("Aluno recebido com sucesso: " + aluno);
 		model.addAttribute("mensagem", "Aluno cadastrado com sucesso!");
 		return "aluno/formAluno";
+	}
+	
+	@ModelAttribute("paises")
+	public List<String> getPaises() {
+		return Arrays.asList(new String[] {"Canada", "Estados Unidos", "Brasil", "França", "Reino Unido"});
+	}
+	
+	@ModelAttribute("linguagens")
+	public List<String> getLinguagens() {
+		return Arrays.asList(new String[] {"Java", "Python", "Java Script", "Ruby", "PHP"});
+	}
+	
+	@ModelAttribute("linguas")
+	public List<String> getLinguas() {
+		return Arrays.asList(new String[] {"Canadense", "Inglês", "Português", "Frances", "Alemão"});
 	}
 	
 }
--- src/main/java/com/webapp/model/Aluno.java
+++ src/main/java/com/webapp/model/Aluno.java
@@ -1,10 +1,15 @@
 package com.webapp.model;
 
+import java.util.List;
+
 public class Aluno {
 	
 	private String nome;
 	private Long matricula;
 	private String login;
+	private String pais;
+	private String linguagem;
+	private List<String> fluencia;
 	
 	public String getNome() {
 		return nome;
@@ -24,9 +29,28 @@ public class Aluno {
 	public void setLogin(String login) {
 		this.login = login;
 	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	public String getLinguagem() {
+		return linguagem;
+	}
+	public void setLinguagem(String linguagem) {
+		this.linguagem = linguagem;
+	}
+	public List<String> getFluencia() {
+		return fluencia;
+	}
+	public void setFluencia(List<String> fluencia) {
+		this.fluencia = fluencia;
+	}
 	@Override
 	public String toString() {
-		return "Aluno [nome=" + nome + ", matricula=" + matricula + ", login=" + login + "]";
+		return "Aluno [nome=" + nome + ", matricula=" + matricula + ", login=" + login + ", pais=" + pais
+				+ ", linguagem=" + linguagem + ", fluencia=" + fluencia + "]";
 	}
 
 }
